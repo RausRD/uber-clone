c59642b16209612bfe142cf3691f3fd314220fe8
RausRD <dunecruslan2@gmail.com>
2024-08-30 07:28:57 +0300

    completing the main flow

--- components/Map.tsx
+++ components/Map.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import { Driver, MarkerData } from "@/types/type";
 import { icons } from "@/constants";
 import { useFetch } from "@/lib/fetch";
+import MapViewDirections from "react-native-maps-directions";
 
 const Map = () => {
   const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
@@ -96,6 +97,33 @@ const Map = () => {
           }
         ></Marker>
       ))}
+      {destinationLongitude && destinationLatitude && (
+        <>
+          <Marker
+            key="destination"
+            coordinate={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            title="Destination"
+            image={icons.pin}
+          />
+
+          <MapViewDirections
+            origin={{
+              latitude: userLatitude!,
+              longitude: userLongitude!,
+            }}
+            destination={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            apikey={process.env.EXPO_PUBLIC_GOOGLE_API_KEY}
+            strokeColor="#0286ff"
+            strokeWidth={2}
+          />
+        </>
+      )}
     </MapView>
   );
 };
